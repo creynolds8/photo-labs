e5e883e8885aac440206d6e4e67fddc13b2f5040
creynolds8 <cameron8.cr@gmail.com>
2024-06-12 13:33:23 -0700

    move sample data to App file and refactor to use props passed in from App

--- frontend/src/components/PhotoListItem.jsx
+++ frontend/src/components/PhotoListItem.jsx
@@ -2,25 +2,13 @@ import React from "react";
 
 import "../styles/PhotoListItem.scss";
 
-
-const sampleDataForPhotoListItem = {
-  id: "1",
-  location: {
-    city: "Montreal",
-    country: "Canada",
-  },
-  imageSource: `${process.env.PUBLIC_URL}/Image-1-Regular.jpeg`,
-  username: "Joe Example",
-  profile: `${process.env.PUBLIC_URL}/profile-1.jpg`,
-};
-
-const PhotoListItem = () => {
+const PhotoListItem = (props) => {
   /* Insert React */
   return <div>
-    <img src={`${sampleDataForPhotoListItem.imageSource}`} alt="" />
-    <img src={`${sampleDataForPhotoListItem.profile}`} alt="" />
-    <p>{sampleDataForPhotoListItem.username}</p>
-    <p>{sampleDataForPhotoListItem.location.city}, {sampleDataForPhotoListItem.location.country}</p>
+    <img src={`${props.imageSource}`} alt="" />
+    <img src={`${props.profile}`} alt="" />
+    <p>{props.username}</p>
+    <p>{props.location.city}, {props.location.country}</p>
   </div>
 };
 
