264bb1c6355d5d71fa2b98bab0eb9d2cb114b05d
creynolds8 <cameron8.cr@gmail.com>
2024-06-12 13:41:18 -0700

    pass key prop into photo list item -> photo id

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -18,7 +18,7 @@ const sampleDataForPhotoListItem = {
 const App = () => {
   return (
     <div className="App">
-      <PhotoListItem {...sampleDataForPhotoListItem}/>
+      <PhotoListItem {...sampleDataForPhotoListItem} key={sampleDataForPhotoListItem.id} />
     </div>
   );
 };
