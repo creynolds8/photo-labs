56d0776d17f3f773fef4f3a20cae3c81c51c89a0
creynolds8 <cameron8.cr@gmail.com>
2024-06-12 14:04:20 -0700

    add classes to allow for styling from provided scss file

--- frontend/src/components/PhotoListItem.jsx
+++ frontend/src/components/PhotoListItem.jsx
@@ -4,11 +4,13 @@ import "../styles/PhotoListItem.scss";
 
 const PhotoListItem = ({ photo }) => {
   /* Insert React */
-  return <div>
-    <img src={`${photo.imageSource}`} alt="" />
-    <img src={`${photo.profile}`} alt="" />
-    <p>{photo.username}</p>
-    <p>{photo.location.city}, {photo.location.country}</p>
+  return <div className="photo-list__item">
+    <img className="photo-list__image" src={`${photo.imageSource}`} alt="" />
+    <div className="photo-list__user-details">
+    <img className="photo-list__user-profile" src={`${photo.profile}`} alt="" />
+    <p className="photo-list__user-details">{photo.username}</p>
+    <p className="photo-list__user-location">{photo.location.city}, {photo.location.country}</p>
+    </div>
   </div>
 };
 
