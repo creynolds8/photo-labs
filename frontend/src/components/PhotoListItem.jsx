38bf5745821253f8a7f7643870dc7671adaf7815
creynolds8 <cameron8.cr@gmail.com>
2024-06-12 14:08:08 -0700

    add div around user info with correct class for better/correct styling as per AI feedback

--- frontend/src/components/PhotoListItem.jsx
+++ frontend/src/components/PhotoListItem.jsx
@@ -4,14 +4,20 @@ import "../styles/PhotoListItem.scss";
 
 const PhotoListItem = ({ photo }) => {
   /* Insert React */
-  return <div className="photo-list__item">
-    <img className="photo-list__image" src={`${photo.imageSource}`} alt="" />
-    <div className="photo-list__user-details">
-    <img className="photo-list__user-profile" src={`${photo.profile}`} alt="" />
-    <p className="photo-list__user-details">{photo.username}</p>
-    <p className="photo-list__user-location">{photo.location.city}, {photo.location.country}</p>
+  return (
+    <div className="photo-list__item">
+      <img className="photo-list__image" src={`${photo.imageSource}`} alt="" />
+      <div className="photo-list__user-details">
+        <img className="photo-list__user-profile" src={`${photo.profile}`} alt=""/>
+        <div className="photo-list__user-info">
+          <div className="photo-list__user-details">{photo.username}</div>
+          <div className="photo-list__user-location">
+            {photo.location.city}, {photo.location.country}
+          </div>
+        </div>
+      </div>
     </div>
-  </div>
+  );
 };
 
 export default PhotoListItem;
