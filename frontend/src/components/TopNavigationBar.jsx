770f08be5efd5799eb56c47275c9f4fba772f4b9
creynolds8 <cameron8.cr@gmail.com>
2024-06-13 13:49:34 -0700

    add TopicList and Favbadge components

--- frontend/src/components/TopNavigationBar.jsx
+++ frontend/src/components/TopNavigationBar.jsx
@@ -1,11 +1,14 @@
 import React from 'react';
 
 import '../styles/TopNavigationBar.scss'
+import TopicList from './TopicList';
+import FavBadge from './FavBadge';
 
 const TopNavigation = () => {
   return (
     <div className="top-nav-bar">
       <span className="top-nav-bar__logo">PhotoLabs</span>
+      <span className='top-nav-bar'><TopicList /><FavBadge /></span>
     </div>
   )
 }
