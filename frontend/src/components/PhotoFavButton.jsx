9f22038e4c6567223cff24240f8c4bc4480a3273
creynolds8 <cameron8.cr@gmail.com>
2024-06-12 17:10:18 -0700

    change setState functionality to allow toggling state

--- frontend/src/components/PhotoFavButton.jsx
+++ frontend/src/components/PhotoFavButton.jsx
@@ -8,7 +8,7 @@ function PhotoFavButton() {
   return (
     <div className="photo-list__fav-icon">
       <div className="photo-list__fav-icon-svg">
-        <div onClick={() => setSelect(true)}>
+        <div onClick={() => setSelect(prev => !prev)}>
           <FavIcon selected={select}/>
         </div>
       </div>
