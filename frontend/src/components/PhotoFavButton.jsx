e87fab11e6547d894fd962c0f0b9fe41d81b416d
creynolds8 <cameron8.cr@gmail.com>
2024-06-12 17:07:36 -0700

    add div around FavIcon with onClick setState functionality

--- frontend/src/components/PhotoFavButton.jsx
+++ frontend/src/components/PhotoFavButton.jsx
@@ -4,10 +4,13 @@ import FavIcon from './FavIcon';
 import '../styles/PhotoFavButton.scss';
 
 function PhotoFavButton() {
+  const [ select, setSelect ] = useState(false)
   return (
     <div className="photo-list__fav-icon">
       <div className="photo-list__fav-icon-svg">
-        {/* Insert React */}
+        <div onClick={() => setSelect(true)}>
+          <FavIcon selected={select}/>
+        </div>
       </div>
     </div>
   );
