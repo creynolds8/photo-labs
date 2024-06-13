767913bb2d48ff6999056410b0e0c3cd722aff83
creynolds8 <cameron8.cr@gmail.com>
2024-06-13 14:16:29 -0700

    import and pass as props to relevant components mock data

--- frontend/src/routes/HomeRoute.jsx
+++ frontend/src/routes/HomeRoute.jsx
@@ -4,11 +4,14 @@ import '../styles/HomeRoute.scss';
 import TopNavigation from 'components/TopNavigationBar';
 import PhotoList from 'components/PhotoList';
 
+import photos from 'mocks/photos';
+import topics from 'mocks/topics';
+
 const HomeRoute = () => {
   return (
     <div className="home-route">
-      <TopNavigation />
-      <PhotoList />
+      <TopNavigation topics={topics} />
+      <PhotoList photos={photos}/>
     </div>
   );
 };
