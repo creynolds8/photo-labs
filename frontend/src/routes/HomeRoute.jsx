cc89ac030d00a8f11425639fc18093d017deb367
creynolds8 <cameron8.cr@gmail.com>
2024-06-13 18:25:09 -0700

    set up useReducer and implement logic in reducer function to check for photo id and add or remove photo id, pass dispatch and state to child components

--- frontend/src/routes/HomeRoute.jsx
+++ frontend/src/routes/HomeRoute.jsx
@@ -1,14 +1,24 @@
-import React from 'react';
+import React, { useReducer } from 'react';
 
 import '../styles/HomeRoute.scss';
 import TopNavigation from 'components/TopNavigationBar';
 import PhotoList from 'components/PhotoList';
 
+const reducer = function(state, action) {
+  if (action.type === 'toggle-fav') {
+    if (state.favPhotos.includes(action.payload)) {
+      return {...state, favPhotos: state.favPhotos.filter(id => id !== action.payload)}
+    }
+    return {...state, favPhotos: [...state.favPhotos, action.payload]}
+  }
+}
+
 const HomeRoute = (props) => {
+  const [ state, dispatch ] = useReducer(reducer, {favPhotos: []})
   return (
     <div className="home-route">
-      <TopNavigation topics={props.topics} />
-      <PhotoList photos={props.photos}/>
+      <TopNavigation topics={props.topics} select={state.favPhotos.length > 0} />
+      <PhotoList photos={props.photos} state={state} dispatch={dispatch}/>
     </div>
   );
 };
