15444e0075f03d345bbf359e0403d31e9f524365
creynolds8 <cameron8.cr@gmail.com>
2024-06-14 13:02:43 -0700

    create handleImageClick function and pass to HomeRoute component, add conditional logic to render modal route

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,16 +1,25 @@
-import React from 'react';
+import React, { useState } from "react";
 
-import './App.scss';
-import HomeRoute from 'routes/HomeRoute';
+import "./App.scss";
+import HomeRoute from "routes/HomeRoute";
 
-import photos from 'mocks/photos';
-import topics from 'mocks/topics';
+import photos from "mocks/photos";
+import topics from "mocks/topics";
+import PhotoDetailsModal from "routes/PhotoDetailsModal";
 
-// Note: Rendering a single component to build components in isolation
 const App = () => {
+  const [modal, setModal] = useState({open: false, photo: null});
+  const handleImageClick = (photo) => {
+    setModal({open: true, photo: photo});
+  };
   return (
     <div className="photo-list">
-      <HomeRoute topics={topics} photos={photos}/>
+      <HomeRoute
+        topics={topics}
+        photos={photos}
+        handleImageClick={handleImageClick}
+      />
+      {modal.open && <PhotoDetailsModal photo={modal.photo}/>}
     </div>
   );
 };
