4d4acf062435dbd7bc84e448389300acad814150
creynolds8 <cameron8.cr@gmail.com>
2024-06-14 13:23:33 -0700

    move onClick functionality to button instead of img

--- frontend/src/routes/PhotoDetailsModal.jsx
+++ frontend/src/routes/PhotoDetailsModal.jsx
@@ -6,8 +6,8 @@ import closeSymbol from '../assets/closeSymbol.svg';
 const PhotoDetailsModal = ({ handleCloseModal }) => {
   return (
     <div className="photo-details-modal">
-      <button className="photo-details-modal__close-button">
-        <img src={closeSymbol} alt="close symbol" onClick={handleCloseModal}/>
+      <button className="photo-details-modal__close-button" onClick={handleCloseModal} >
+        <img src={closeSymbol} alt="close symbol" />
       </button>
     </div>
   )
