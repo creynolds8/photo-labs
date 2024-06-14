3d53f7a443b999836ba2cc51fc52929796a3ed7b
creynolds8 <cameron8.cr@gmail.com>
2024-06-14 13:34:04 -0700

    destructure photo object and log photo

--- frontend/src/routes/PhotoDetailsModal.jsx
+++ frontend/src/routes/PhotoDetailsModal.jsx
@@ -3,7 +3,8 @@ import React from 'react';
 import '../styles/PhotoDetailsModal.scss'
 import closeSymbol from '../assets/closeSymbol.svg';
 
-const PhotoDetailsModal = ({ handleCloseModal }) => {
+const PhotoDetailsModal = ({ photo, handleCloseModal }) => {
+  console.log(photo);
   return (
     <div className="photo-details-modal">
       <button className="photo-details-modal__close-button" onClick={handleCloseModal} >
