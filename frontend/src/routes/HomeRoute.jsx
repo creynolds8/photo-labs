e47d6d0002f58f6c592c2a248a37d17151cd9856
creynolds8 <cameron8.cr@gmail.com>
2024-06-14 15:17:32 -0700

    remove useReducer and associated code, take state and dispatch as props and pass down to child components

--- frontend/src/routes/HomeRoute.jsx
+++ frontend/src/routes/HomeRoute.jsx
@@ -1,24 +1,14 @@
-import React, { useReducer } from 'react';
+import React from 'react';
 
 import '../styles/HomeRoute.scss';
 import TopNavigation from 'components/TopNavigationBar';
 import PhotoList from 'components/PhotoList';
 
-const reducer = function(state, action) {
-  if (action.type === 'toggle-fav') {
-    if (state.favPhotos.includes(action.payload)) {
-      return {...state, favPhotos: state.favPhotos.filter(id => id !== action.payload)}
-    }
-    return {...state, favPhotos: [...state.favPhotos, action.payload]}
-  }
-}
-
 const HomeRoute = (props) => {
-  const [ state, dispatch ] = useReducer(reducer, {favPhotos: []})
   return (
     <div className="home-route">
-      <TopNavigation topics={props.topics} select={state.favPhotos.length > 0} />
-      <PhotoList photos={props.photos} state={state} dispatch={dispatch} handleImageClick={props.handleImageClick}/>
+      <TopNavigation topics={props.topics} select={props.state.favPhotos.length > 0} />
+      <PhotoList photos={props.photos} state={props.state} dispatch={props.dispatch} handleImageClick={props.handleImageClick}/>
     </div>
   );
 };
