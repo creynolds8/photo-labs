64c3f506d09f54ca5eb73b541f6e86dc37cd34aa
creynolds8 <cameron8.cr@gmail.com>
2024-06-14 13:00:31 -0700

    call handleImageClick on image click

--- frontend/src/components/PhotoListItem.jsx
+++ frontend/src/components/PhotoListItem.jsx
@@ -3,13 +3,13 @@ import React from "react";
 import "../styles/PhotoListItem.scss";
 import PhotoFavButton from "./PhotoFavButton";
 
-const PhotoListItem = ({ photo, isFavPhoto, dispatch }) => {
+const PhotoListItem = ({ photo, isFavPhoto, dispatch, handleImageClick }) => {
   return (
     <div className="photo-list__item">
       <PhotoFavButton isFavPhoto={isFavPhoto} handleClick={() => dispatch({payload: photo.id, type: "toggle-fav"})}/>
-      <img className="photo-list__image" src={`${photo.urls.regular}`} alt="" />
+      <img className="photo-list__image" src={`${photo.urls.regular}`} alt="" onClick={() => handleImageClick(photo)} />
       <div className="photo-list__user-details">
-        <img className="photo-list__user-profile" src={`${photo.user.profile}`} alt=""/>
+        <img className="photo-list__user-profile" src={`${photo.user.profile}`} alt="" />
         <div className="photo-list__user-info">
           <div>{photo.user.name}</div>
           <div className="photo-list__user-location">
