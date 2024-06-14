6db1fb565423d718d0ccc401d64544b9ae8a61c1
creynolds8 <cameron8.cr@gmail.com>
2024-06-14 15:18:52 -0700

    adjust div positions and class names to fix styling, use photolist component instead of array of photolistitems, take in and pass down needed props

--- frontend/src/routes/PhotoDetailsModal.jsx
+++ frontend/src/routes/PhotoDetailsModal.jsx
@@ -3,14 +3,16 @@ import React from "react";
 import "../styles/PhotoDetailsModal.scss";
 import closeSymbol from "../assets/closeSymbol.svg";
 import PhotoFavButton from "components/PhotoFavButton";
-import PhotoListItem from "components/PhotoListItem";
+import PhotoList from "components/PhotoList";
 
-
-const PhotoDetailsModal = ({ photo, handleCloseModal }) => {
-  const similarPhotosArray = Object.values(photo.similar_photos)
-  const similarPhotoComponents = similarPhotosArray.map(photo => {
-  return <PhotoListItem key={photo.id} photo={photo} />
-  })
+const PhotoDetailsModal = ({
+  photo,
+  handleCloseModal,
+  state,
+  dispatch,
+  handleImageClick,
+}) => {
+  const similarPhotosArray = Object.values(photo.similar_photos);
 
   return (
     <div className="photo-details-modal">
@@ -20,28 +22,31 @@ const PhotoDetailsModal = ({ photo, handleCloseModal }) => {
       >
         <img src={closeSymbol} alt="close symbol" />
       </button>
-      <PhotoFavButton />
-      <img
-        className="photo-details-modal__image"
-        src={photo.urls.regular}
-      />
-      <div className="photo-details-modal__header">
-        <div className="photo-details-modal__photographer-details">
-          <img
-            className="photo-details-modal__photographer-profile"
-            src={photo.user.profile}
-          />
-          <div className="photo-details-modal__photographer-info">
-            {photo.user.name}
-            <div className="photo-details-modal__photographer-location">
-              {photo.location.city}, {photo.location.country}
+
+      <div className="photo-details-modal__images">
+        <PhotoFavButton />
+        <img className="photo-details-modal__image" src={photo.urls.regular} />
+        <div className="photo-details-modal__header">
+          <div className="photo-details-modal__photographer-details">
+            <img
+              className="photo-details-modal__photographer-profile"
+              src={photo.user.profile}
+            />
+            <div className="photo-details-modal__photographer-info">
+              {photo.user.name}
+              <div className="photo-details-modal__photographer-location">
+                {photo.location.city}, {photo.location.country}
+              </div>
             </div>
           </div>
         </div>
       </div>
-      <div className="photo-details-modal__images">
-        {similarPhotoComponents}
-      </div>
+      <PhotoList
+        photos={similarPhotosArray}
+        state={state}
+        dispatch={dispatch}
+        handleImageClick={handleImageClick}
+      />
     </div>
   );
 };
