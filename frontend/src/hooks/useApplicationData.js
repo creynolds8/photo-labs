61d4ece8dff7bb0e65badc6651b854896a11d782
creynolds8 <cameron8.cr@gmail.com>
2024-06-14 18:27:28 -0700

    change reducer function to switch case to allow default case

--- frontend/src/hooks/useApplicationData.js
+++ frontend/src/hooks/useApplicationData.js
@@ -1,27 +1,40 @@
-import React, { useState, useReducer } from 'react'
+import React, { useState, useReducer } from "react";
 
 import photos from "mocks/photos";
 
-export default function useApplicationData () {
-  const reducer = function(state, action) {
-    if (action.type === 'toggle-fav') {
-      if (state.favPhotos.includes(action.payload)) {
-        return {...state, favPhotos: state.favPhotos.filter(id => id !== action.payload)}
-      }
-      return {...state, favPhotos: [...state.favPhotos, action.payload]}
+export default function useApplicationData() {
+  const reducer = function (state, action) {
+    switch (action.type) {
+      case "toggle-fave":
+        if (action.type === "toggle-fav") {
+          if (state.favPhotos.includes(action.payload)) {
+            return {
+              ...state,
+              favPhotos: state.favPhotos.filter((id) => id !== action.payload),
+            };
+          }
+          return { ...state, favPhotos: [...state.favPhotos, action.payload] };
+        }
+        default:
+          return state;
     }
-  }
+  };
 
-  const [ state, updateToFavPhotoIds ] = useReducer(reducer, {favPhotos: []})
-  const [modal, setModal] = useState({open: false, photo: null});
+  const [state, updateToFavPhotoIds] = useReducer(reducer, { favPhotos: [] });
+  const [modal, setModal] = useState({ open: false, photo: null });
   const setPhotoSelected = (clickedPhoto) => {
-    const foundPhoto = photos.find((photo) => photo.id === clickedPhoto.id)
-    setModal({open: true, photo: foundPhoto});
+    const foundPhoto = photos.find((photo) => photo.id === clickedPhoto.id);
+    setModal({ open: true, photo: foundPhoto });
   };
   const onClosePhotoDetailsModal = () => {
-    setModal({open: false, photo: null})
-  }
+    setModal({ open: false, photo: null });
+  };
 
-
-  return {state, modal, updateToFavPhotoIds, setPhotoSelected, onClosePhotoDetailsModal}
+  return {
+    state,
+    modal,
+    updateToFavPhotoIds,
+    setPhotoSelected,
+    onClosePhotoDetailsModal,
+  };
 }
