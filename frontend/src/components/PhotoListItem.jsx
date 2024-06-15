a111c5e99542b94ce200db9388ecea784471c4bd
creynolds8 <cameron8.cr@gmail.com>
2024-06-14 19:13:11 -0700

    change handle image click to dispatch with action type open modal and payload of photo clicked

--- frontend/src/components/PhotoListItem.jsx
+++ frontend/src/components/PhotoListItem.jsx
@@ -7,7 +7,7 @@ const PhotoListItem = ({ photo, isFavPhoto, dispatch, handleImageClick }) => {
   return (
     <div className="photo-list__item">
       <PhotoFavButton isFavPhoto={isFavPhoto} handleClick={() => dispatch({payload: photo.id, type: "toggle-fav"})}/>
-      <img className="photo-list__image" src={photo.urls.regular} alt="" onClick={() => handleImageClick(photo)} />
+      <img className="photo-list__image" src={photo.urls.regular} alt="" onClick={() => dispatch({payload: photo, type: 'open-modal'})} />
       <div className="photo-list__user-details">
         <img className="photo-list__user-profile" src={photo.user.profile} alt="" />
         <div className="photo-list__user-info">
