e2b2c5cbf3683f8707a62e1219ef9fe6c666eab7
creynolds8 <cameron8.cr@gmail.com>
2024-06-14 18:20:51 -0700

    copy over app functionality from App.jsx and rename variables to match Compass

--- frontend/src/hooks/useApplicationData.js
+++ frontend/src/hooks/useApplicationData.js
@@ -1,8 +1,27 @@
 import React, { useState, useReducer } from 'react'
 
+import photos from "mocks/photos";
+
 export default function useApplicationData () {
-  
+  const reducer = function(state, action) {
+    if (action.type === 'toggle-fav') {
+      if (state.favPhotos.includes(action.payload)) {
+        return {...state, favPhotos: state.favPhotos.filter(id => id !== action.payload)}
+      }
+      return {...state, favPhotos: [...state.favPhotos, action.payload]}
+    }
+  }
+
+  const [ state, updateToFavPhotoIds ] = useReducer(reducer, {favPhotos: []})
+  const [modal, setModal] = useState({open: false, photo: null});
+  const setPhotoSelected = (clickedPhoto) => {
+    const foundPhoto = photos.find((photo) => photo.id === clickedPhoto.id)
+    setModal({open: true, photo: foundPhoto});
+  };
+  const onClosePhotoDetailsModal = () => {
+    setModal({open: false, photo: null})
+  }
 
 
-  return {state, updateToFavPhotoIds, setPhotoSelected, onClosePhotoDetailsModal}
+  return {state, modal, updateToFavPhotoIds, setPhotoSelected, onClosePhotoDetailsModal}
 }
