67351cb9e0f4614beb3f3f48ce5bddcb9295e017
creynolds8 <cameron8.cr@gmail.com>
2024-06-14 19:14:05 -0700

    remove unnecessary props both in and out of component

--- frontend/src/components/PhotoList.jsx
+++ frontend/src/components/PhotoList.jsx
@@ -3,7 +3,7 @@ import React from "react";
 import "../styles/PhotoList.scss";
 import PhotoListItem from "./PhotoListItem";
 
-const PhotoList = ({ photos, state, dispatch, handleImageClick }) => {
+const PhotoList = ({ photos, state, dispatch }) => {
   const photoComponents = photos.map(photo => {
     const select = state.favPhotos.includes(photo.id);
     return (<PhotoListItem
@@ -11,7 +11,6 @@ const PhotoList = ({ photos, state, dispatch, handleImageClick }) => {
     photo={photo}
     isFavPhoto={select}
     dispatch={dispatch}
-    handleImageClick={handleImageClick}
     />)})
   return (
     <ul className="photo-list">
