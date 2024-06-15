a9b5a3014ba9ceafb9e77195648ae978aab82394
creynolds8 <cameron8.cr@gmail.com>
2024-06-14 18:21:59 -0700

    remove app functionality, destructure useApplicationData and pass needed values to child components as props

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,45 +1,40 @@
 import React, { useState, useReducer } from "react";
 
 import "./App.scss";
-import HomeRoute from "routes/HomeRoute";
-
-import photos from "mocks/photos";
+  
 import topics from "mocks/topics";
+
+import HomeRoute from "routes/HomeRoute";
 import PhotoDetailsModal from "routes/PhotoDetailsModal";
-
-
-const reducer = function(state, action) {
-  if (action.type === 'toggle-fav') {
-    if (state.favPhotos.includes(action.payload)) {
-      return {...state, favPhotos: state.favPhotos.filter(id => id !== action.payload)}
-    }
-    return {...state, favPhotos: [...state.favPhotos, action.payload]}
-  }
-}
+import useApplicationData from "hooks/useApplicationData";
 
 const App = () => {
-  const [ state, dispatch ] = useReducer(reducer, {favPhotos: []})
-  const [modal, setModal] = useState({open: false, photo: null});
-  const handleImageClick = (clickedPhoto) => {
-    const foundPhoto = photos.find((photo) => photo.id === clickedPhoto.id)
-    setModal({open: true, photo: foundPhoto});
-  };
-  const handleCloseModal = () => {
-    setModal({open: false, photo: null})
-  }
+  const {
+    state,
+    modal,
+    updateToFavPhotoIds,
+    setPhotoSelected,
+    onClosePhotoDetailsModal,
+  } = useApplicationData();
+
   return (
     <div className="photo-list">
       <HomeRoute
         state={state}
-        dispatch={dispatch}
+        dispatch={updateToFavPhotoIds}
         topics={topics}
         photos={photos}
-        handleImageClick={handleImageClick}
+        handleImageClick={setPhotoSelected}
       />
-      {modal.open && <PhotoDetailsModal photo={modal.photo} handleCloseModal={handleCloseModal} handleImageClick={handleImageClick}
-      state={state}
-      dispatch={dispatch}
-      />}
+      {modal.open && (
+        <PhotoDetailsModal
+          photo={modal.photo}
+          handleCloseModal={onClosePhotoDetailsModal}
+          handleImageClick={setPhotoSelected}
+          state={state}
+          dispatch={updateToFavPhotoIds}
+        />
+      )}
     </div>
   );
 };
