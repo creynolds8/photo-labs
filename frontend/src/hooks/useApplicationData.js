433b701b212a90f5e97531874559c4118ddb5c1d
creynolds8 <cameron8.cr@gmail.com>
2024-06-14 18:09:44 -0700

    import React, set up structure for function

---
+++ frontend/src/hooks/useApplicationData.js
@@ -0,0 +1,8 @@
+import React, { useState, useReducer } from 'react'
+
+export default function useApplicationData () {
+  
+
+
+  return {state, updateToFavPhotoIds, setPhotoSelected, onClosePhotoDetailsModal}
+}
