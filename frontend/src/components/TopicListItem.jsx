cb9285b67a930589be6f7bdfada96c3e7bbffe58
creynolds8 <cameron8.cr@gmail.com>
2024-06-15 19:50:18 -0700

    take in dispatch as prop and set up onclick handling with dispatch to filter by topic id

--- frontend/src/components/TopicListItem.jsx
+++ frontend/src/components/TopicListItem.jsx
@@ -2,10 +2,10 @@ import React from "react";
 
 import "../styles/TopicListItem.scss";
 
-const TopicListItem = ({topic}) => {
+const TopicListItem = ({topic, dispatch}) => {
   return (
       <div className="topic-list__item">
-        <span>{topic.title}</span>
+        <span onClick={() => dispatch({type: 'get-photos-by-topic', payload: topic.id})}>{topic.title}</span>
       </div>
   );
 };
