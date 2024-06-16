5bf5d3a7bcd42db36f37d84f85b788137618ca19
creynolds8 <cameron8.cr@gmail.com>
2024-06-16 16:58:23 -0700

    create basic error message component and import styles

---
+++ frontend/src/components/ErrorMessage.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+import "../styles/ErrorMessage.scss"
+
+const ErrorMessage = function({error}) {
+  return (
+    <div className="error-message">
+      <h2>We are very sorry but an error has occured</h2>
+        <p>{error}</p>
+    </div>
+  )
+};
+
+export default ErrorMessage
