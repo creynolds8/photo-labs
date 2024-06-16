26007687d9028a1d91e3dfd8edabddc24b7dc5a9
creynolds8 <cameron8.cr@gmail.com>
2024-06-15 19:49:42 -0700

    take in and pass down dispatch as prop for use in filtering by topic id

--- frontend/src/components/TopicList.jsx
+++ frontend/src/components/TopicList.jsx
@@ -3,8 +3,8 @@ import React from "react";
 import "../styles/TopicList.scss";
 import TopicListItem from "./TopicListItem";
 
-const TopicList = ({topics}) => {
-  const topicComponents = topics.map(topic => <TopicListItem key={topic.id} topic={topic} />)
+const TopicList = ({ topics, dispatch }) => {
+  const topicComponents = topics.map(topic => <TopicListItem key={topic.id} topic={topic} dispatch={dispatch} />)
   return (
     <div className="top-nav-bar__topic-list">
       {topicComponents}
