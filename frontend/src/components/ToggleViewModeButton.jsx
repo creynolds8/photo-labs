395831669c6b0a9fc089eb82e504323619015b45
creynolds8 <cameron8.cr@gmail.com>
2024-06-17 15:11:52 -0700

    create basic component with button and onclick functionality

---
+++ frontend/src/components/ToggleViewModeButton.jsx
@@ -0,0 +1,11 @@
+const ToggleViewModeButton = ({ dispatch }) => {
+  return (
+    <div className="view-mode-button">
+      <button onClick={() => dispatch({type: 'toggle--view-mode'})}>
+        click to toggle view mode
+      </button>
+    </div>
+  );
+};
+
+export default ToggleViewModeButton;
