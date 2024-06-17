8effbe846ad54f84aa42ac40e3a1609986f778b8
creynolds8 <cameron8.cr@gmail.com>
2024-06-17 15:14:14 -0700

    import and call view mode button component

--- frontend/src/components/TopNavigationBar.jsx
+++ frontend/src/components/TopNavigationBar.jsx
@@ -4,12 +4,14 @@ import "../styles/TopNavigationBar.scss";
 
 import TopicList from "./TopicList";
 import FavBadge from "./FavBadge";
+import ToggleViewModeButton from "./ToggleViewModeButton";
 
 const TopNavigation = ({ select, topics, dispatch }) => {
   return (
     <div className="top-nav-bar">
       <span className="top-nav-bar__logo">PhotoLabs</span>
       <TopicList topics={topics} dispatch={dispatch} />
+      <ToggleViewModeButton dispatch={dispatch} />
       <FavBadge isFavPhotoExist={select} />
     </div>
   );
