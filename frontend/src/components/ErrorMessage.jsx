1075afdafbc04f0aeaff2b10c6af8108f26f91f0
creynolds8 <cameron8.cr@gmail.com>
2024-06-16 17:09:23 -0700

    add punctuation

--- frontend/src/components/ErrorMessage.jsx
+++ frontend/src/components/ErrorMessage.jsx
@@ -5,7 +5,7 @@ import "../styles/ErrorMessage.scss"
 const ErrorMessage = function({error}) {
   return (
     <div className="error-message">
-      <h2>We are very sorry but an error has occured</h2>
+      <h2>We are very sorry, but an error has occured:</h2>
         <p>{error}</p>
     </div>
   )
